80f23f99ee3c2b30b8399342c6d8f45e63e9c44e
Faraday <faraday@buffer.finance>
2024-02-07 11:26:36 +0530

    cleanup

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,50 +1,18 @@
 import { createConfig } from "@ponder/core";
 import { http } from "viem";
-import { BufferBinaryOptions } from "./abis/BufferBinaryOption";
 import { BufferRouter } from "./abis/BufferRouter";
-import { Config } from "./abis/Config";
 export default createConfig({
   networks: {
     blastSepolia: {
       chainId: 168587773,
       transport: http(process.env.PONDER_RPC_URL_1),
     },
+    arbitrumMainnet: {
+      chainId: 42161,
+      transport: http(process.env.ARBITRUM_MAINNET_RPC_URL),
+    },
   },
   contracts: {
-    BufferBinaryOptions: {
-      network: "blastSepolia",
-      abi: BufferBinaryOptions,
-      startBlock: 1004456,
-      maxBlockRange: 10000,
-      filter: {
-        event: [
-          "CreateOptionsContract",
-          "Create",
-          "Expire",
-          "Exercise",
-          "Pause",
-        ],
-      },
-    },
-    OptionsConfig: {
-      network: "blastSepolia",
-      abi: Config,
-      startBlock: 1004456,
-      maxBlockRange: 10000,
-      filter: {
-        event: [
-          "UpdateMinFee",
-          "UpdateCreationWindowContract",
-          "UpdateCircuitBreakerContract",
-          "UpdateOptionStorageContract",
-          "UpdatePlatformFee",
-          "UpdateSettlementFeeDisbursalContract",
-          "UpdateSf",
-          "UpdatetraderNFTContract",
-          "UpdateStrikeStepSize",
-        ],
-      },
-    },
     RouterContract: {
       network: "blastSepolia",
       abi: BufferRouter,
